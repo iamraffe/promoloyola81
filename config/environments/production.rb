fb336082cdf6d01e7f03558636e86b1149c73c46
Rafa Ramirez <raffe90@gmail.com>
2017-05-13 02:37:13 +0200

    Adds SMTP & removes registerable from devise

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,6 +86,16 @@ Rails.application.configure do
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
   end
 
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'gmail.com',
+    user_name:            ENV.fetch("GMAIL_SMPT_EMAIL"),
+    password:             ENV.fetch("GMAIL_SMPT_PASSWORD"),
+    authentication:       :plain,
+    enable_starttls_auto: true
+  }
+
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
