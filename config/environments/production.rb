e0306a58acec95a2247b3206b130c431aab72bff
Rafa Ramirez <raffe90@gmail.com>
2017-05-13 02:38:14 +0200

    Adds typo fix SMTP

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,8 +90,8 @@ Rails.application.configure do
     address:              'smtp.gmail.com',
     port:                 587,
     domain:               'gmail.com',
-    user_name:            ENV.fetch("GMAIL_SMPT_EMAIL"),
-    password:             ENV.fetch("GMAIL_SMPT_PASSWORD"),
+    user_name:            ENV.fetch("GMAIL_SMTP_EMAIL"),
+    password:             ENV.fetch("GMAIL_SMTP_PASSWORD"),
     authentication:       :plain,
     enable_starttls_auto: true
   }
